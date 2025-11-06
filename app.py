12cd6a1520c2e496a20ba786df1610dc0b07e623
Jesse Morrison <jessemorrison@mu-602839.dhcp.missouri.edu>
2025-11-06 12:21:22 -0600

    Displaying posts - saved

--- app.py
+++ app.py
@@ -23,8 +23,15 @@ def get_db_connection():
 # use the app.route() decorator to create a Flask view function called index()
 @app.route('/')
 def index():
+    #get a connection to the database
+    conn = get_db_connection()
+    #execute query to read all posts from post table
+    posts = conn.exectute("SELECT * FROM posts").fetchall()
+    #close connection
+    conn.close()
+    #send posts to index.html template to be displayed
     
-    return "<h1>Welcome to My Blog</h1>"
+    return render_template('index.html', posts=posts)
 
 
 # route to create a post
